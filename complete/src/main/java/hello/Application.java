fb9eb397b19ba832ff3147cfd048d98ab0a9dbea
yolgun <yu_olgun@hotmail.com>
2017-01-19 21:59:13 +0100

    rest, metrics

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,10 +1,29 @@
 package hello;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class Application {
+    public static MetricRegistry metrics = new MetricRegistry();
+    static {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                                                  .convertRatesTo(TimeUnit.SECONDS)
+                                                  .convertDurationsTo(TimeUnit.MILLISECONDS)
+                                                  .build();
+        JmxReporter jmxReporter = JmxReporter.forRegistry(metrics)
+                                       .convertRatesTo(TimeUnit.SECONDS)
+                                       .convertDurationsTo(TimeUnit.MILLISECONDS)
+                                       .build();
+        reporter.start(5, TimeUnit.SECONDS);
+        jmxReporter.start();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.codahale.metrics.Timer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,35 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private final Timer timer = Application.metrics.timer("timer");
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+        Timer.Context time = timer.time();
+        Greeting result = new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+        time.stop();
+        return result;
+    }
+
+    @RequestMapping("/v2/greeting")
+    public Greeting greetingv2(@RequestParam(value="name", defaultValue="World") String name) {
+        Timer.Context time = timer.time();
+        Object[] arr = new Object[100000];
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Greeting result = new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+        time.stop();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        time.stop();
+        return result;
     }
 }
